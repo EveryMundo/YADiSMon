98410f3d13b080c65c28ea33483117237881383c
Daniel Santana <daniel@everymundo.com>
2016-10-25 15:35:30 -0400

    closes #2 adds the ability to monitor multiple mount points

--- disk-space-checker.sh
+++ disk-space-checker.sh
@@ -1,23 +1,24 @@
 #!/bin/bash
 
 DIRECTORY=$(dirname $0)
-echo DIRECTORY:$DIRECTORY
+
 cd $DIRECTORY
 
-exec >> logs/disk-space-checker.log
-exec 2>&1
+# Redirects STDOUT and STDERR to the log file
+# exec >> logs/disk-space-checker.log
+# exec 2>&1
 
 THRESHOLD=$1
 
 export N=$(df | grep -P '/$' | grep -Po '\d+%' | grep -Po '\d+');
 
 if [ "$N" -gt "$THRESHOLD" ]; then
-  echo "$N > $THRESHOLD"
+  echo "$(date) $N > $THRESHOLD"
 
   export NVM_DIR="$HOME/.nvm"
   [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
 
-  df -h | grep -P '/$' | ./disk-space-to-slack.js
+  df -h | ./disk-space-to-slack.js "$THRESHOLD"
 else
   echo "$(date) | $N < $THRESHOLD ok"
 fi;
--- disk-space-to-slack.js
+++ disk-space-to-slack.js
@@ -1,37 +1,63 @@
 #!/usr/bin/env node
 'use strict';
+/* eslint-disable no-console */
+
+// Loads .env file with environmental variables
 require('dotenv').load();
 
-/* eslint-disable no-console */
 const
-  slack   = require('./lib/slack'),
-  execute = require('./lib/execute');
+  assert    = require('assert'),
+  slack     = require('./lib/slack'),
+  execute   = require('./lib/execute'),
+  THRESHOLD = Math.abs(process.argv[2] || process.env.THRESHOLD);
 
-process.stdin.setEncoding('utf8');
+// THE APP STARTS RUNNING RIGHT HERE
+init();
 
-let input = '';
+function init() {
+  assert(process.argv[2] || 'THRESHOLD' in process.env, 'THRESHOLD variable/parameter not set');
+  assert(THRESHOLD <= 100, `THRESHOLD [${THRESHOLD}] is greater than 100`);
 
-process.stdin.on('readable', () => {
-  var chunk = process.stdin.read();
-  if (chunk !== null) {
-    input += chunk;
-  }
-});
+  processStandardInput();
+}
 
-process.stdin.on('end', () => {
-  process.stdout.write(`data: ${input}`);
+function processStandardInput() {
+  let input = '';
 
+  process.stdin.setEncoding('utf8');
+
+  process.stdin.on('readable', () => {
+    var chunk = process.stdin.read();
+    if (chunk !== null) {
+      input += chunk;
+    }
+  });
+
+  process.stdin.on('end', () => {
+    process.stdout.write(`input:\n${input}\n`);
+    const
+      VALID_INPUT_REGEXP = new RegExp(process.env.VALID_INPUT_REGEXP || '^\\/'),
+      lines       = input.split('\n'),
+      inputHeader = lines.shift(),
+      validLines  = lines.filter(_ => VALID_INPUT_REGEXP.test(_));
+
+    validLines.forEach(checkThreshold(inputHeader));
+  });
+}
+
+const checkThreshold = inputHeader => input => {
   const [filesystem, size, used, avail, use, mountPoint] = input.split(/\s+/);
+
+  if (+(use.replace(/\D+/g,'')) < THRESHOLD) return;
 
   execute('hostname', [], (err, response) => {
     const hostname = ('' + (err || response)).replace('\n', '');
 
-    sendMessage(hostname, filesystem, size, used, avail, use, mountPoint);
+    sendMessage(hostname, filesystem, size, used, avail, use, mountPoint, input, inputHeader);
   });
-  process.stdout.write('end');
-});
+};
 
-function sendMessage(hostname, filesystem, size, used, avail, use, mountPoint) {
+function sendMessage(hostname, filesystem, size, used, avail, use, mountPoint, input, inputHeader) {
   const date = new Date().toJSON().replace('T','* at *').replace(/\.\w+$/,'');
   console.log({date});
   const
@@ -39,18 +65,18 @@ function sendMessage(hostname, filesystem, size, used, avail, use, mountPoint) {
     token    = process.env.SLACK_TOKEN,
     channel  = process.env.SLACK_CHANNEL  || '#general',
     username = process.env.SLACK_USERNAME || 'YADiSMon',
-    attachments = [{
+    attachment = {
       icon_emoji: process.env.SLACK_EMOJI || ':ghost:',
       color:    '#36a64f',
       fallback: message,
       pretext:  `Event happend on *${date}*`,
-      // text:     'Optional text that appears *within* the attachment',
+      text:     `*${use}* of use is over the configured threshold of *${THRESHOLD}*%`,
 
-      // author_name: process.env.APP_NAME || 'APP_NAME not configured',
-      author_icon: 'https://slack.com/img/icons/app-57.png',
+      author_name: process.env.SLACK_AUTHOR_NAME || hostname,
+      author_icon: process.env.SLACK_AUTHOR_ICON, // EXAMPLE: 'https://slack.com/img/icons/app-57.png',
 
-      // title:      process.env.SLACK_TITLE || 'SLACK_TITLE not configured',
-      // title_link: 'https://api.slack.com/',
+      title:      process.env.SLACK_TITLE,
+      title_link: process.env.SLACK_TITLE_LINK,
       mrkdwn_in:  ['text', 'pretext', 'fields'],
       fields: [
           {'title': 'Priority',     'value': 'High'  ,   'short': true},
@@ -61,14 +87,18 @@ function sendMessage(hostname, filesystem, size, used, avail, use, mountPoint) {
           {'title': 'Size',         'value': size,       'short': true},
           {'title': 'Used',         'value': used,       'short': true},
           {'title': 'Available',    'value': avail,      'short': true},
-          {'title': 'df -h output', 'value': '```'+input+'```'   , 'short': false},
       ],
 
-      image_url: 'http://my-website.com/path/to/image.jpg',
-      thumb_url: 'http://example.com/path/to/thumb.png'
-    }];
+      image_url:   process.env.SLACK_IMAGE_URL,
+      thumb_url:   process.env.SLACK_THUMB_URL,
+      footer:      process.env.SLACK_FOOTER      || 'github.com/EveryMundo/YADiSMon',
+      footer_icon: process.env.SLACK_FOOTER_ICON || 'https://pbs.twimg.com/profile_images/585493660952879105/0g1omKlr_normal.png',
+    };
+
+  if (process.env.INCLUDE_RAW_INPUT)
+    attachment.fields.push({'title': 'df -h output', 'value': '```'+inputHeader+'\n'+input+'```'   , 'short': false});
 
   slack
-  .sendMessageToSlack(token, message, channel, username, attachments)
+  .sendMessageToSlack(token, message, channel, username, [attachment])
   .then(console.log('Done!'));
 }
